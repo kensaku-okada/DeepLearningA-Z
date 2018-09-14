a74ccf0557f18e84e3d99259405a99673d6c4753
Kensaku Okada <kensakuokada@email.arizona.edu>
2018-09-14 17:50:56 +0900

    directory changed to shorten the path

---
+++ Deep_Learning_A_Z/Volume 2 - Unsupervised Deep Learning/Part 4 - Self Organizing Maps (SOM)/Section 16 - Building a SOM/Self_Organizing_Maps/mySon.py
@@ -0,0 +1,22 @@
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+
+print(os.getcwd())
+
+
+dataset = pd.read_csv('Credit_Card_Applications.csv')
+
+
+
+
+
+
+
+
+
+
+
